390f1bba320ca5a658ae885c2c9182861910ba4e
Josegrcia02 <josefol881@gmail.com>
2025-11-26 19:27:15 +0100

    config ajuste

--- config/config.php
+++ config/config.php
@@ -1,9 +1,8 @@
 <?php
     $baseUrl = getenv('BASE_URL');
-    if(!$baseUrl) {
+    if(empty($baseUrl)) {
         $baseUrl = 'http://localhost:8084'; // Tu puerto local del docker-compose
     }
     // Elimina la barra final si existe para estandarizar
     define("BASE_URL", rtrim($baseUrl, '/'));
-?>
 ?>
